61dee9a1d973033b4e62a550600013f2a33009cd
Sanca <matiassancassani@gmail.com>
2024-05-02 22:34:09 -0300

    Eliminar Productos

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -33,30 +33,14 @@ socket.on('productos', (productos) => {
         botonDelete = document.querySelectorAll(".delete");
         botonDelete.forEach(button => {
         button.addEventListener("click", eliminarDelcarrito);
-        const title = document.getElementById('title').value;
-        const description = document.getElementById('description').value;
-        const price = document.getElementById('price').value;
-        const code = document.getElementById('code').value;
-        const stock = document.getElementById('stock').value;
-        const category = document.getElementById('category').value;
-
-        const producto = {
-            title: title,
-            description: description,
-            price: price,
-            code: code,
-            stock: stock,
-            category: category
-        };
-        socket.emit('deleteProduct', producto);
         });
-       
         
     }
     function eliminarDelcarrito (e) {
         const idButton = e.currentTarget.id;
         const index = productos.findIndex(productos => productos.id === idButton);
         productos.splice(index, 1);
+        socket.emit('deleteProduct', idButton);
         cargarProductos();
         
     }    
